d8f276df091cb22e00988bfe927c366e5c651e64
wls <jin042078@hs.ac.kr>
2026-08-18 10:49:26 +0900

    fix: GitHub Pages 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Choco-app-0814/',
+  base: '/Choco-Calendar/',
   plugins: [react()],
 })
